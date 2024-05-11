c67d02c9a03c340d03309942d4fdb88d5847b993
IvanXh0 <ivan.apostolovski@gmail.com>
2024-05-11 20:41:52 +0200

    feat: add framer motion

--- client/consts/apiConfig.ts
+++ client/consts/apiConfig.ts
@@ -1,9 +1,13 @@
+const ENV = process.env.NODE_ENV;
+const DEVELOPMENT_URL = process.env.NEXT_PUBLIC_DEVELOPMENT_URL;
+const PRODUCTION_URL = process.env.NEXT_PUBLIC_PRODUCTION_URL;
+
+if (!ENV || !DEVELOPMENT_URL || !PRODUCTION_URL)
+  throw new Error("Missing env vars");
+
 const API_BASE_URLS: Record<string, string> = {
-  development: "http://localhost:3000/api",
-  production: "tba",
+  development: DEVELOPMENT_URL,
+  production: PRODUCTION_URL,
 };
 
-const { NODE_ENV } = process.env;
-
-export const API_BASE_URL =
-  API_BASE_URLS[NODE_ENV] ?? API_BASE_URLS.development;
+export const API_BASE_URL = API_BASE_URLS[ENV] ?? API_BASE_URLS.development;
