1ce42b90b5120b741b582466b5ad492edb7b48e2
IvanXh0 <ivan.apostolovski@gmail.com>
2024-05-12 20:03:40 +0200

    feat: deploying backend

--- server/src/const/dataSource.ts
+++ server/src/const/dataSource.ts
@@ -1,14 +1,38 @@
 import { DataSource } from 'typeorm';
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_CONNECTION_STRING,
+  NODE_ENV,
+} = process.env;
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
+const isProduction = NODE_ENV === 'production';
+
+const developmentVariables = {
   host: DB_HOST || 'localhost',
   port: DB_PORT ? Number(DB_PORT) : 5432,
   username: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+};
+
+const productionVariables = {
+  url: DB_CONNECTION_STRING,
+};
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: DB_CONNECTION_STRING,
+  ...(isProduction ? productionVariables : developmentVariables),
+  // host: DB_HOST || 'localhost',
+  // port: DB_PORT ? Number(DB_PORT) : 5432,
+  // username: DB_USER,
+  // password: DB_PASSWORD,
+  // database: DB_NAME,
   synchronize: true,
   logging: false,
   entities: ['src/entity/**/*.ts'],
