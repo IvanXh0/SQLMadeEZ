b3e99481bdf8f0d976265f9b178a758f78bcbeaa
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2024-12-07 22:36:40 +0100

    chore: delete old server

--- server/src/routes/table.route.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { TableController } from '../controllers/table.controller';
-
-export const tableRouter = Router();
-
-tableRouter.get('/:userId', TableController.getTables);
--- server/src/utils/modifySqlForReturning.ts
+++
@@ -1,8 +0,0 @@
-export const modifySqlForReturning = (sql: string, type: string): string => {
-  const shouldModify = ['insert', 'update', 'delete'].includes(type);
-
-  if (shouldModify && !sql.toLowerCase().includes('returning')) {
-    return `${sql.trim()} RETURNING *`;
-  }
-  return sql;
-};
